8dc927824247672bc15b022b34ba03dfa27097dd
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-21 20:53:45 -0300

    created vue base

---
+++ mini-portifolio/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+  <div class="flex-flex-col min-h-screen bg-gray-100">
+    <div class="main container mx-auto px-4 py-6"></div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
