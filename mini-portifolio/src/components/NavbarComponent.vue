ea3a1d2ba36e1bca844a437de2d2d2e9ce532c1a
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-21 22:10:48 -0300

    feat: added base Navbar

---
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -0,0 +1,5 @@
+<template>
+    <nav class="bg-cyan-500 text-black py-4 px-8 flex justify-between-items-center">
+        <h1 class="font-bold text-lg">Kennedy's Portifolio</h1>
+    </nav>
+</template>
