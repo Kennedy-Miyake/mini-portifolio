f2abbfc3060a7abfbe4184c4a43d97ea99e5df21
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 04:41:21 -0300

    feat: added toggle menu function to open and close the menu at the click of a button

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -9,7 +9,19 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+  name: 'NavbarComponent',
+
+  setup() {
+    const isOpen = ref(false);
+
+    function toggleMenu() {
+      isOpen.value = !isOpen.value;
+    }
+    return {
+      isOpen,
+      toggleMenu
     }
   }
 }
