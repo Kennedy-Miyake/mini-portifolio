b0c35f77685db88629769430dfb187d3cf30a0ef
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 05:50:00 -0300

    mod: added the code with my information

--- mini-portifolio/src/views/HomePageView.vue
+++ mini-portifolio/src/views/HomePageView.vue
@@ -8,5 +8,26 @@
         <h1>Olá, Eu sou o Kennedy!</h1>
         <h4>Desenvolvedor Backend</h4>
       </div>
+      <div>
+        <!--barra de clique-->
+        <div>developer.c</div>
+
+        <!--Código em C-->
+        <div>
+          <code>int main(int argc, char* argv[]) {</code>
+          <code>const char perfil[][] = [</code>
+          <code>["nome", "Kennedy"],</code>
+          <code>["profissão", "Desenvolvedor Backend"],</code>
+          <code>["skills", "C, C++, C#, Rust, MySql, Git,</code>
+          <code>Docker, Linux"]</code>
+          <code>];</code>
+          <code>const int trabalhoDuro = 1;</code>
+          <code>const int rapidoNoAprendizado = 1;</code>
+          <code>const int resolvedorDeProblemas = 1;</code>
+          <code>const int anosDeExperiencia = 0;</code>
+          <code>return 0;</code>
+          <code>}</code>
+        </div>
+      </div>
     </main>
 </template>
