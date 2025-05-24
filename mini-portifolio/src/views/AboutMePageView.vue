4c60e9765b469ca0c5d6d974794109ad91ca49da
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 14:25:19 -0300

    feat: created the entire structure of the about me

---
+++ mini-portifolio/src/views/AboutMePageView.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <div>
+      <!-- Título -->
+      <h1>Sobre Mim</h1>
+      <!-- Descrição e foto -->
+      <p>Meu nome é Kennedy Miyake, sou desenvolvedor backend.</p>
+      <p>Sou apaixonado por tecnologia desde que me entendo por gente.</p>
+      <p>Tenho paixão pela inovação e avanços tecnológicos.</p>
+    </div>
+    <div>
+      <!-- Formação Acadêmica -->
+      <h1>Formação Acadêmica</h1>
+      <!-- Logo da Unimar -->
+      <img src="" alt=""/>
+      <h4>2024/2026</h4>
+    </div>
+    <!-- Habilidades -->
+    <div>
+      <h1>Habilidades</h1>
+    </div>
+    <div class="bg-gray-500 w-90 h-50">
+      <div>
+        <img src="" alt="">
+        <h6>Desenvolvimento Backend</h6>
+      </div>
+    </div>
+    <div class="bg-gray-500 w-90 h-50">
+      <div>
+        <img src="" alt="">
+        <h6>Desenvolvimento Frontend</h6>
+      </div>
+    </div>
+    <div class="bg-gray-500 w-90 h-50">
+      <div>
+        <img src="" alt="">
+        <h6>Cloud & DevOps</h6>
+      </div>
+    </div>
+    <div class="bg-gray-500 w-90 h-50">
+      <div>
+        <img src="" alt="">
+        <h6>Ferramentas & Tecnologias</h6>
+      </div>
+    </div>
+  </main>
+</template>
