b5fb5e787723c831235eb4de9ba7a76451133507
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 20:42:58 -0300

    mod: leaving the navbar fixed and adding the routes to the menu button

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="relative fixed top-0 w-full bg-[#ff4950] text-black py-4 px-8 flex z-50">
+  <nav class="fixed top-0 w-full bg-[#ff4950] text-black py-4 px-8 flex z-50">
     <img src="../assets/icons/bars-svgrepo-com.svg" alt="bars" class="w-8 h-8 cursor-pointer" @click="toggleMenu"/>
     <h1 class="font-bold text-lg text-[#fffffc] justify-center items-center ml-5">Kennedy's Portifolio</h1>
     <div class="flex relative ml-auto space-x-4">
@@ -19,13 +19,13 @@
          class="absolute top-full left-0 mt-2 w-60 bg-gray-100 shadow-lg rounded-2xl z-10 ml-6">
       <ul>
         <li class="block px-4 py-2 hover:bg-gray-200 rounded-t-2xl">
-          <a href="#home">Home</a>
+          <a href="/">Home</a>
         </li>
         <li class="block px-4 py-2 hover:bg-gray-200">
-          <a href="#about">Sobre Mim</a>
+          <a href="/about">Sobre Mim</a>
         </li>
         <li class="block px-4 py-2 hover:bg-gray-200 rounded-b-2xl">
-          <a href="#contact">Contato</a>
+          <a href="/contact">Contato</a>
         </li>
       </ul>
     </div>
