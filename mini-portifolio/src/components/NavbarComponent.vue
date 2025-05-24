c51b5d37d2277a80e8fb2bd48a33a6873296cd49
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 03:56:48 -0300

    mod: changed the svg bar link to a downloaded svg

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -1,8 +1,6 @@
 <template>
     <nav class="bg-[#ff4950] text-black py-4 px-8 flex">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-      </svg>
+      <img src="../assets/icons/bars-svgrepo-com.svg" alt="bars" class="size-8" />
       <h1 class="font-bold text-lg text-[#fffffc] justify-center items-center ml-5">Kennedy's Portifolio</h1>
       <img src="../assets/icons/linkedin-svgrepo-com.svg" alt="linkedin" class="size-8" />
       <img src="../assets/icons/github-142-svgrepo-com.svg" alt="github" class="size-8" />
