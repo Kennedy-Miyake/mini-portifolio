99e68f4bfe53843cf6e0adf11e1526900c836591
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 04:48:49 -0300

    mod: rounding off the menu and its items

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -8,15 +8,15 @@
 
     <!-- Menu -->
     <div v-if="isOpen"
-         class="absolute top-full left-0 mt-2 w-60 bg-gray-100 shadow-lg rounded-2xl z-10">
+         class="absolute top-full left-0 mt-2 w-60 bg-gray-100 shadow-lg rounded-2xl z-10 ml-6">
       <ul>
-        <li class="block px-4 py-2 hover:bg-gray-200">
+        <li class="block px-4 py-2 hover:bg-gray-200 rounded-t-2xl">
           <a href="#home">Home</a>
         </li>
         <li class="block px-4 py-2 hover:bg-gray-200">
           <a href="#about">Sobre Mim</a>
         </li>
-        <li class="block px-4 py-2 hover:bg-gray-200">
+        <li class="block px-4 py-2 hover:bg-gray-200 rounded-b-2xl">
           <a href="#contact">Contato</a>
         </li>
       </ul>
