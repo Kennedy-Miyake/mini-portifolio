f5b88ef1bbf9df84571bb8ca0f99f3b30f420427
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 04:01:16 -0300

    feat: click on the bars button. button will open a menu with options for selection

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -1,9 +1,19 @@
 <template>
     <nav class="bg-[#ff4950] text-black py-4 px-8 flex">
-      <img src="../assets/icons/bars-svgrepo-com.svg" alt="bars" class="size-8" />
+      <img src="../assets/icons/bars-svgrepo-com.svg" alt="bars" class="size-8" @click="handleClick"/>
       <h1 class="font-bold text-lg text-[#fffffc] justify-center items-center ml-5">Kennedy's Portifolio</h1>
       <img src="../assets/icons/linkedin-svgrepo-com.svg" alt="linkedin" class="size-8" />
       <img src="../assets/icons/github-142-svgrepo-com.svg" alt="github" class="size-8" />
       <img src="../assets/icons/instagram-svgrepo-com.svg" alt="instagram" class="size-8" />
     </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      alert('Menu icon clicked!');
+    }
+  }
+}
+</script>
