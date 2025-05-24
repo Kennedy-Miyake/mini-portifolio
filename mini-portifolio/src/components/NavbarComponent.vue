93266598bc9138380c9b50615558d19b08d5ee14
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 03:12:35 -0300

    css: changing the navbar colors to a new color palette

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-cyan-500 text-black py-4 px-8 flex justify-between-items-center">
-        <h1 class="font-bold text-lg">Kennedy's Portifolio</h1>
+    <nav class="bg-[#ff4950] text-black py-4 px-8 flex justify-between-items-center">
+        <h1 class="font-bold text-lg text-[#fffffc]">Kennedy's Portifolio</h1>
     </nav>
 </template>
