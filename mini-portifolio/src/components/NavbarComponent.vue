bdbdf3920d621f6b9bc8473fca3475e51e6581a1
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 03:48:42 -0300

    mod: added svgs to navbar
    sgvs github, instagram e linkedIn

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -4,5 +4,8 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
       </svg>
       <h1 class="font-bold text-lg text-[#fffffc] justify-center items-center ml-5">Kennedy's Portifolio</h1>
+      <img src="../assets/icons/linkedin-svgrepo-com.svg" alt="linkedin" class="size-8" />
+      <img src="../assets/icons/github-142-svgrepo-com.svg" alt="github" class="size-8" />
+      <img src="../assets/icons/instagram-svgrepo-com.svg" alt="instagram" class="size-8" />
     </nav>
 </template>
