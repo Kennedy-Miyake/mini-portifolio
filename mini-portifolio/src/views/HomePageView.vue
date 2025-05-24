c5ff503c8f25f0157fc4053d706ee9da1b5b33b5
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 07:01:18 -0300

    feat: tab style added

--- mini-portifolio/src/views/HomePageView.vue
+++ mini-portifolio/src/views/HomePageView.vue
@@ -1,33 +1,27 @@
-<script setup>
-</script>
+<style>
+.tab4 {
+  display: inline-block;
+  margin-left: 40px;
+}
 
+.tab8 {
+  display: inline-block;
+  margin-left: 80px;
+}
+</style>
 <template>
-    <main>
-      <div class="text-[#fffffc]">
         <p class="text-1xl">printf("Hello, World!");</p>
         <h1 class="text-6xl text-[#ff4950] font-bold">Olá, Eu sou o Kennedy!</h1>
         <h4 class="text-2xl font-medium">Desenvolvedor Backend</h4>
       </div>
-      <div>
         <!--barra de clique-->
-        <div>developer.c</div>
 
         <!--Código em C-->
-        <div>
           <code>int main(int argc, char* argv[]) {</code>
-          <code>const char perfil[][] = [</code>
-          <code>["nome", "Kennedy"],</code>
-          <code>["profissão", "Desenvolvedor Backend"],</code>
-          <code>["skills", "C, C++, C#, Rust, MySql, Git,</code>
-          <code>Docker, Linux"]</code>
-          <code>];</code>
-          <code>const int trabalhoDuro = 1;</code>
-          <code>const int rapidoNoAprendizado = 1;</code>
-          <code>const int resolvedorDeProblemas = 1;</code>
-          <code>const int anosDeExperiencia = 0;</code>
-          <code>return 0;</code>
           <code>}</code>
         </div>
       </div>
     </main>
 </template>
+  <script setup lang="ts">
+  </script>
