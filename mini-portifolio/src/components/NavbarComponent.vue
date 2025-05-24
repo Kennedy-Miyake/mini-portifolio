d5a4e76d9db7a326d306eb7d77448b524a418794
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-24 03:35:12 -0300

    mod: added an svg to list the page options. adjusted the items inside the navbar

--- mini-portifolio/src/components/NavbarComponent.vue
+++ mini-portifolio/src/components/NavbarComponent.vue
@@ -1,5 +1,8 @@
 <template>
-    <nav class="bg-[#ff4950] text-black py-4 px-8 flex justify-between-items-center">
-        <h1 class="font-bold text-lg text-[#fffffc]">Kennedy's Portifolio</h1>
+    <nav class="bg-[#ff4950] text-black py-4 px-8 flex">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+      </svg>
+      <h1 class="font-bold text-lg text-[#fffffc] justify-center items-center ml-5">Kennedy's Portifolio</h1>
     </nav>
 </template>
